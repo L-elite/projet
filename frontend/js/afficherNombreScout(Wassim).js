4091cd7242e273234f4da6041fc726ada95ebf87
WassimBouhdid <64103126+WassimBouhdid@users.noreply.github.com>
2020-05-17 16:13:42 +0200

    Add files via upload

--- frontend/js/afficherNombreScout(Wassim).js
+++ frontend/js/afficherNombreScout(Wassim).js
@@ -1,7 +1,7 @@
-window.addEventListener("load", afficherNombreScout);
+window.addEventListener("load", afficherNombreScout); //La méthode addEventListener() met en place une fonction à appeler chaque fois que l'événement spécifié est remis à la cible.
 
 function afficherNombreScout() {
-    let xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest(); //permet d'envoyer une nouvelle requête HTTP
     xhr.open("get", "nombreDeScout");
     xhr.onload = callbackAffichageNombreScout;
     xhr.send();
@@ -10,12 +10,12 @@ function afficherNombreScout() {
 function callbackAffichageNombreScout() {
     let tableau = JSON.parse(this.responseText);
     let affichage = "<ul id=table>"
-    let jesaispas = 0;
+    let total = 0;
     for (let e of tableau) {
-        jesaispas += e.nbrScoutPatrouille;
+        total += e.nbrScoutPatrouille;
         let div = "<li value=" + e.idPatrouille +" id=" + e.idPatrouille + ">" + e.nomDePatrouille + " : " + e.nbrScoutPatrouille + "</li>";
         affichage += div;
     }
-    affichage += "<li>Total :" + String(jesaispas) + "</li>";
+    affichage += "<li>Total :" + String(total) + "</li>";
     document.getElementById("nombreDeScout").innerHTML = affichage;
 }
