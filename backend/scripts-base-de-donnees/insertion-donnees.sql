a63512f4126f9b96317994d8142e60747e3d706f
LouisJanquart <64102269+LouisJanquart@users.noreply.github.com>
2020-05-18 13:05:45 +0200

    Update insertion-donnees.sql

--- backend/scripts-base-de-donnees/insertion-donnees.sql
+++ backend/scripts-base-de-donnees/insertion-donnees.sql
@@ -1,11 +1,11 @@
------ Insertion donn�es -----
+----- Insertion données -----
 
 insert into scouts
 (scoutPrenom, scoutNom, patrouille, role, totem, telephone, mail, dateNaissance)
 values 
 ('Bastien', 'Sabeau', 2, 1, 'Basindo', '0499/99.99.99', 'basindo@gmail.com', '2003-05-01'),
 ('Martin', 'Malfroid', 3, 1, 'Coryi', '0499/99.99.99', 'coryi@gmail.com', '2003-05-01'),
-('Al�andro', 'Gerardin', 2, 2, 'Manati', '0499/99.99.99', 'manati@gmail.com', '2003-05-01'),
+('Aléandro', 'Gerardin', 2, 2, 'Manati', '0499/99.99.99', 'manati@gmail.com', '2003-05-01'),
 ('Alexandre', 'Delory', 1, 2, 'Miriki', '0499/99.99.99', 'miriki@gmail.com', '2003-05-01'),
 ('Mael', 'Albertuccio', 1, 3, 'Springbok', '0499/99.99.99', 'springbok@gmail.com', '2003-05-01' ),
 ('Adrien', 'Florins', 1, 3, 'Orignal', '0499/99.99.99', 'orignal@gmail.com', '2003-05-01'),
@@ -18,8 +18,8 @@ values
 ('Gabriel', 'Lefevre', 3, 3, null, '0499/99.99.99', 'gabriel@gmail.com', '2003-05-01'),
 ('Logan', 'Lorent', 2, 3, null, '0499/99.99.99', 'logan@gmail.com', '2003-05-01'),
 ('Florian', 'Maes', 3, 3, 'Aonyx', '0499/99.99.99', 'aonyx@gmail.com', '2003-05-01'),
-('R�nathan', 'Nonnveiler', 3, 2, 'Brolga', '0499/99.99.99', 'brolga@gmail.com', '2003-05-01'),
-('F�lix', 'Roelands', 1, 3, null, '0499/99.99.99', 'felix@gmail.com', '2003-05-01');
+('Rénathan', 'Nonnveiler', 3, 2, 'Brolga', '0499/99.99.99', 'brolga@gmail.com', '2003-05-01'),
+('Félix', 'Roelands', 1, 3, null, '0499/99.99.99', 'felix@gmail.com', '2003-05-01');
 
 --
 
@@ -40,3 +40,36 @@ values
 ('Patrouille de Coryi');
 
 --
+
+insert into badges
+(badgeLib)
+values
+('Badge campeur'),
+('Badge sportif'),
+('Badge naturaliste'),
+('Badge artiste'),
+('Badge correpondant'),
+('Badge troubadour'),
+('Badge bricoleur'),
+('Badge frère de tous'),
+('Badge pilote'),
+('Badge intendant');
+
+--
+
+insert into scouts_badges
+(scoutId, badgeId)
+values
+(1, 1),
+(1, 2),
+(1, 10),
+(2, 1),
+(2, 2),
+(2, 6),
+(3, 10),
+(3, 6),
+(4, 1),
+(4, 6),
+(5, 2),
+(6, 2),
+(7, 4);
