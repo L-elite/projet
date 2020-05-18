f5e37a07586c6f17c16ee20af85b738c51c4e21d
LouisJanquart <64102269+LouisJanquart@users.noreply.github.com>
2020-05-18 12:57:49 +0200

    Update common.js

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -4,11 +4,14 @@ function afficherCarte(card) {
     document.getElementById("inscription").style.display = "none";
     document.getElementById("listing").style.display = "none";
     document.getElementById("recherche").style.display = "none";
+    document.getElementById("badges").style.display = "none";
     if (card === 'inscription') {
         document.getElementById("inscription").style.display = "table";
     } else if (card === 'listing') {
         document.getElementById("listing").style.display = "table";
     } else if (card === 'recherche') {
         document.getElementById("recherche").style.display = "table";
+    } else if (card === 'badges') {
+        document.getElementById("badges").style.display = "table";
     }
 }
