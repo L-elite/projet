72ca3ea5401080c5533f8efad3eac2390719eff0
LouisJanquart <64102269+LouisJanquart@users.noreply.github.com>
2020-06-01 15:23:35 +0200

    Delete supprimer.js

--- frontend/js/supprimer.js
+++
@@ -1,9 +0,0 @@
-function supprimer() {
-    let xhr = new XMLHttpRequest;
-    let id = document.querySelector(".fiche table").id
-    let url = "supprimer?Id=" + id;
-    xhr.open("get", url);
-    xhr.send();
-    window.location.reload(false);
-    alert("ce scout a bien été supprimé de la liste !!");
-}
