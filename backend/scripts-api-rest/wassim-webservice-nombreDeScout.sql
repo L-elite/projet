d8850e8217972925f137d846c82bfcac6674ecbe
LouisJanquart <64102269+LouisJanquart@users.noreply.github.com>
2020-06-01 19:13:57 +0200

    Update wassim-webservice-nombreDeScout.sql

--- backend/scripts-api-rest/wassim-webservice-nombreDeScout.sql
+++ backend/scripts-api-rest/wassim-webservice-nombreDeScout.sql
@@ -1,12 +1,10 @@
 CREATE PROCEDURE "DBA"."proc_afficherNombreScout"()
 result(idPatrouille integer, nomDePatrouille char(255), nbrScoutPatrouille INTEGER )
 BEGIN 
-
-select patrId,patrNom,count(scoutId) 
-from scouts join patrouilles
-    on scouts.patrouille = patrouilles.patrId
-GROUP BY patrId,patrNom
-
+    select patrId, patrLib, count(scoutId) 
+    from scouts
+    join patrouilles on scouts.scoutPatrouille = patrouilles.patrId
+    GROUP BY patrId, patrLib
 END
 
 --
